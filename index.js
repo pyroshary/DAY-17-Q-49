e6f88b1ca050aed03ed74cb80bca83b0b3e620f8
pyroshary <sheharyarghulamrasool@gmail.com>
2024-03-21 01:24:27 +0500

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function loghobbies() {
+    var hobbies = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        hobbies[_i] = arguments[_i];
+    }
+    hobbies.forEach(function (hobby) {
+        console.log("I enjoy ".concat(hobby));
+    });
+}
+loghobbies("EATING", "football", "Reading");
---
+++ index.ts
@@ -0,0 +1,3 @@
+function loghobbies(...hobbies: string[]){hobbies.forEach(hobby =>{console.log(`I enjoy ${hobby}`);
+})}
+loghobbies("EATING","football","Reading");
